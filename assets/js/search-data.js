5fbcf4da55d579dca312e0044af06b903c625429
rydalch <rydalch@users.noreply.github.com>
2025-11-12 22:30:51 +0000

    Deploying to gh-pages from @ rydalch/rydalch.github.io@0d191a959f9b8923701b5994132daa53102150f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,11 +45,6 @@ ninja.data = [{
           description: "knowledgebase of current cyber risk resources",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
